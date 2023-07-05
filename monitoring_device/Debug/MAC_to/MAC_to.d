09fac34484fe1dfffa197424846bb533e71ecfe3
andresoliva <olivaandres93@gmail.com>
2023-07-05 18:56:12 -0300

    V1.1

--- monitoring_device/Debug/MAC_to/MAC_to.d
+++
@@ -1,6 +0,0 @@
-# FIXED
-
-MAC_to/MAC_to.o: G:/texas_instruments_proyects/libraries/MAC_to/MAC_to.c \
- G:/texas_instruments_proyects/libraries/MAC_to/MAC_to.h
-
-G:/texas_instruments_proyects/libraries/MAC_to/MAC_to.h:
